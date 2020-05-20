5faf2948dc1864b7064e6d835837c64da3fbd397
FaabScheeren <f.scheeren@outlook.com>
2020-05-20 20:29:08 +0200

    Setup input form and location sharing with events acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,58 @@
-io();
+const socket = io();
+
+// Form elements
+const $messageForm = document.querySelector("form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+
+// Location elements
+const $locationButton = document.querySelector("#send-location");
+
+socket.on("message", (message) => {
+  console.log(`${message}`);
+});
+
+socket.on("updateMessage", (input) => {
+  console.log(`This is the message ${input}`);
+});
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Disable form
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  // Ways to select input element by name
+  // const input = document.querySelector('input[name="inputfield"]').value;
+  const input = e.target.elements.inputfield.value;
+  socket.emit("formSubmit", input, (error) => {
+    // Enable form
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The message was deliverd!");
+  });
+});
+
+$locationButton.addEventListener("click", (e) => {
+  $locationButton.setAttribute("disabled", "disabled");
+
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const location = {
+      longitude: position.coords.longitude,
+      latitude: position.coords.latitude,
+    };
+
+    socket.emit("sendLocation", location, (message) => {
+      $locationButton.removeAttribute("disabled");
+      console.log(message);
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,8 +13,36 @@ const publicPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicPath));
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New websocket connection.");
+  socket.emit("message", "Welcome, to this connection!");
+
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("formSubmit", (input, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(input)) {
+      return callback("Profanity is not allowed!");
+    }
+
+    io.emit("updateMessage", input);
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
+  });
+
+  socket.on("sendLocation", (location, callback) => {
+    // console.log(location);
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${location.latitude},${location.longitude}`
+    );
+
+    callback("Location shared!");
+  });
 });
 
 server.listen(port, () => {
